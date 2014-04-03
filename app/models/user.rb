43d088b576c1f043d345f0a06aeb47e989c3dad2
Elomar Souza <elomar@elomar.me>
2014-04-03 12:48:04 -0300

    implementa available_questions, mas falha quando question não tem resposta alguma

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
   has_many :answers
   has_many :questions, through: :answers
+
+  def available_questions
+    Question.includes(:answers).where("answers.user_id != ?", id).references(:answers)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "questions brings answered questions" do
+    assert_equal [questions(:one)], users(:answered_one).questions
+  end
+
+  test "available_questions brings unanswered questions" do
+    assert_includes users(:answered_one).available_questions, questions(:two)
+  end
+
+  test "available_questions brings questions with no answer" do
+    assert_includes users(:answered_one).available_questions, questions(:three)
+  end
 end
